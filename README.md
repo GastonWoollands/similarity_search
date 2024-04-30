2bb80745610de9e5d87cac6c59b145b1d590ead8
Gaston Woollands <g.woollands@gmail.com>
2024-04-30 12:28:18 +0200

    similarity_search init

---
+++ README.md
@@ -0,0 +1,45 @@
+# Similarity Search Function
+
+This repository contains a Python function named `similarity_search` that performs a semantic search on a list of query texts against a list of known texts using a given model.
+
+## Function Overview
+
+The `similarity_search` function takes four required parameters:
+
+- `text_q`: A list of query texts to be searched.
+- `texts_k`: A list of known texts against which the query texts are searched.
+- `model`: A model used to encode the texts.
+- `top_k`: The number of top similar texts to return for each query text (default is 3).
+- `score`: The minimum score required for a text to be included in the results (default is 0).
+
+The function returns a dictionary where each key is a query text and the value is a dictionary containing the top similar texts from the known texts and their corresponding similarity scores.
+
+## Usage
+
+You can use the `similarity_search` function by calling it with the required parameters. For example:
+
+```python
+from similarity_search import similarity_search
+
+query_texts = ["text1", "text2", "text3"]
+known_texts = ["text1a", "text1b", "text2a", "text2b", "text3a", "text3b"]
+model = YourModelHere  # Replace with your actual model
+
+results = similarity_search(query_texts, known_texts, model, top_k=1, score=0.5)
+
+
+You can then access the results dictionary to see the top similar texts for each query text:
+print(results["text1"])  # Output: {"text1a"  {"text1b": 0.6}
+print(results["text2"])  # Output: {"text2a"  {"text2b": 0.7}
+print(results["text3"])  # Output: {"text3a"  {"text3b": 0.6}
+
+```
+
+# Instalation
+
+To use the similarity_search function, you will need to install PyTorch and your chosen model. You can do this using pip:
+pip install torch
+
+# License
+
+This repository is licensed under the MIT License. See the LICENSE file for details.
---
+++ similarity_search/similarity.py
@@ -0,0 +1,47 @@
+from sentence_transformers import SentenceTransformer
+from sentence_transformers.utils import semantic_search
+from typing import List
+
+def similarity_search(texts_q: List[str], texts_k: List[str], model, top_k = 3, score= 0):
+    """
+    Performs a semantic search on a list of query texts against a list of known texts using a given model.
+
+    Args:
+        text_q (List[str]): A list of query texts to be searched.
+        texts_k (List[str]): A list of known texts against which the query texts are searched.
+        model: A model used to encode the texts.
+        top_k (int, optional): The number of top similar texts to return for each query text. Defaults to 3.
+        score (int, optional): The minimum score required for a text to be included in the results. Defaults to 0.
+
+    Returns:
+        dict: A dictionary where each key is a query text and the value is a dictionary containing the top similar texts from the known texts and their corresponding similarity scores.
+    """
+
+    query = texts_q
+    keys  = texts_k
+
+    response_dict = {}
+
+    embeddings_q = model.encode(query)
+    embeddings_k = model.encode(keys)
+
+    search = semantic_search(embeddings_q, embeddings_k, top_k=top_k)
+
+    for j in range(len(search)):
+        response_dict[query[j]] = {}
+        sorted_results = []
+
+        for i in range (0, top_k):
+            if search[j][i].get('score') >= score:
+                sorted_results.append((keys[search[j][i].get('corpus_id')], search[j][i].get('score')))
+            else:
+                pass
+       
+        sorted_results = sorted(sorted_results, key=lambda x: x[1], reverse=True)
+
+        for result in sorted_results:
+            response_dict[query[j]][result[0]] = result[1]
+       
+        response_dict[query[j]] = {k: response_dict[query[j]][k] for k, v in sorted_results}
+   
+    return response_dict
